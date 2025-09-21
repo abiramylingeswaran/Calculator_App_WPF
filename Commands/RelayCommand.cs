bc96906d572f95ce660d0256b0d3404f364770fd
abiramylingeswaran <144706138+abiramylingeswaran@users.noreply.github.com>
2025-09-21 23:34:34 +0530

    Update RelayCommand.cs

--- Commands/RelayCommand.cs
+++ Commands/RelayCommand.cs
@@ -9,22 +9,21 @@ namespace CalculatorApp.Commands
 {
     public class RelayCommand : ICommand
     {
-        private readonly Action<object> _execute;
+    private readonly Action<object> _execute;
 
-        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
-        {
-            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
-        }
+    public RelayCommand(Action<object> execute)
+    {
+        _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+    }
 
-        public bool CanExecute(object parameter)
-        {
-            // Always true
-            return true;
-        }
+    public bool CanExecute(object parameter) => true;
+   
 
-        public void Execute(object parameter)
-        {
-            _execute(parameter);
-        }      
+    public void Execute(object parameter)
+    {
+        _execute(parameter);
+    }
+
+    public event EventHandler CanExecuteChanged;
     }
 }
